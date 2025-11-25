cab0ba41407f66cf16a4e20073347fc13debb01b
Morar Timotei <timotei-cristian.morar@endava.com>
2025-11-25 18:22:54 +0200

    still working on dev infra

---
+++ infra/env/dev/main.tf
@@ -0,0 +1,19 @@
+module "network" {
+  source     = "../../modules/network"
+}
+
+module "alb" {
+  source        = "../../modules/alb"
+  vpc_id        = module.network.vpc_id
+  public_subnet_ids    = module.network.public_subnet_ids
+}
+
+module "ecs_fargate" {
+  source            = "../../modules/ecs_fargate"
+  aws_vpc           = module.network.vpc_id
+  environment = "dev"
+  subnet_id = module.network.public_subnet_ids[0]
+  alb_sg = module.alb.alb_sg
+  target_group_arn = module.alb.alb_tg
+  image = var.image
+}
---
+++ infra/env/dev/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # Configuration to pickup the terraform state from S3 using DynamoDB lock
+  backend "s3" {
+    bucket = "timo-remote-terraform-state-bucket"
+    key = "env/dev/terraform.tfstate"
+    region = "us-west-1"
+    dynamodb_table = "timo-remote-terraform-state-table"
+  }
+}

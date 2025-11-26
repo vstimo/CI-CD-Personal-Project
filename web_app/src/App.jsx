e5fa0d8846e48eae9752741279aaa2625c7422b9
Morar Timotei <timotei-cristian.morar@endava.com>
2025-11-26 18:49:02 +0200

    first commit on timo-change branch

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Timo modification v2</h1>
+        <h1>Timo modification BEST MODIFICATION</h1>
       </header>
       <MainContent />
     </>

9691d8632ed9a58ae5119bf2f6aae042391a620e
Morar Timotei <timotei-cristian.morar@endava.com>
2025-11-26 18:43:14 +0200

    working on deploy122

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Timo modification</h1>
+        <h1>Timo modification v2</h1>
       </header>
       <MainContent />
     </>

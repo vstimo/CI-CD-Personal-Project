faf7cf2d7c2c47923d3cf937c2bd38e85f351df8
Morar Timotei <timotei-cristian.morar@endava.com>
2025-12-02 10:17:54 +0200

    infra test, prod + option deploy env

--- infra/env/test/main.tf
+++ infra/env/test/main.tf
@@ -1,9 +1,19 @@
-resource "aws_vpc" "main" {
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
+module "network" {
+  source     = "../../modules/network"
+}
 
-    tags = {
-        Name = "timo-vpc"
-    }
+module "alb" {
+  source        = "../../modules/alb"
+  vpc_id        = module.network.vpc_id
+  public_subnet_ids    = module.network.public_subnet_ids
+}
+
+module "ecs_fargate" {
+  source            = "../../modules/ecs_fargate"
+  aws_vpc           = module.network.vpc_id
+  environment = "test"
+  subnet_id = module.network.public_subnet_ids[0]
+  alb_sg = module.alb.alb_sg
+  target_group_arn = module.alb.alb_tg
+  image = var.image
 }

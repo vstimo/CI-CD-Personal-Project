19ce1969ed8650ae254a41e9f14fc15e37585b6a
Morar Timotei <timotei-cristian.morar@endava.com>
2025-12-03 14:51:27 +0200

    no safety risk

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -2,10 +2,6 @@ import MainContent from "./components/MainContent";
 import logo from "./assets/images/logo.png";
 
 function App() {
-  // Simulate user-controlled input from the URL
-  const params = new URLSearchParams(window.location.search);
-  const unsafeContent = params.get("msg") || "Hello from safe default";
-
   return (
     <>
       <header>
@@ -14,13 +10,6 @@ function App() {
         </div>
         <h1>cristi-branch: small change</h1>
       </header>
-
-      {/* INTENTIONALLY UNSAFE: user-controlled HTML */}
-      <div
-        id="unsafe-block"
-        dangerouslySetInnerHTML={{ __html: unsafeContent }}
-      />
-
       <MainContent />
     </>
   );

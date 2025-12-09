ac1ac3029e6a33625fd2415961e7972d12d9fa8c
Morar Timotei <timotei-cristian.morar@endava.com>
2025-12-09 15:20:40 +0200

    app.jsx

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>cristi-branch: small change</h1>
+        <h1>This is main branch</h1>
       </header>
       <MainContent />
     </>
